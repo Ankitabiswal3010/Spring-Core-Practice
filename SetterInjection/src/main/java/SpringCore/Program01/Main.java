367c5c4f562c29461184144f586b0eb7985ee3a3
Ankita <ankita30biswal@gmail.com>
2026-07-20 12:02:01 +0530

    Add Setter Injection project

---
+++ SetterInjection/src/main/java/SpringCore/Program01/Main.java
@@ -0,0 +1,15 @@
+package SpringCore.Program01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		ApplicationContext context=new ClassPathXmlApplicationContext("SpringCore/Program01/application-context.xml");
+		Employee bean=context.getBean("Employee",Employee.class);
+		bean.display();       
+	}
+
+}
